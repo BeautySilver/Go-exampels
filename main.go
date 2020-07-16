a8ead6e03402155e86c43c0afc3b2f0db3f1304c
BeautySilver <32017851+BeautySilver@users.noreply.github.com>
2020-07-16 16:50:45 +0300

    Enter Data

--- main.go
+++ main.go
@@ -2,27 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func productsHandler( writer http.ResponseWriter, request *http.Request){
-	vars := mux.Vars(request)
-	id:=vars["id"]
-	cat:= vars["cat"]
-	response:=fmt.Sprintf("Product category=%s id=%s", cat, id)
-	fmt.Fprint(writer, response)
-}
-
-func indexHandler(writer http.ResponseWriter, request *http.Request){
-	fmt.Fprint(writer, "Index")
-}
 func main(){
 
-	router:= mux.NewRouter()
-    router.HandleFunc("/products/category/{id:[0-9]+}", productsHandler)
-	router.HandleFunc("/articles/{id:[0-9]}", productsHandler)
-	router.HandleFunc("/", indexHandler)
-	http.Handle("/", router)
-    http.ListenAndServe("localhost:8181", nil)
+http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	http.ServeFile(writer, request, "form.html")
+})
+http.HandleFunc("/postform", func(writer http.ResponseWriter, request *http.Request) {
+	name:= request.FormValue("username")
+	age:=request.FormValue("userage")
+
+	fmt.Fprintf(writer,"Name: %s Age: %s", name, age)
+})
+http.ListenAndServe("localhost:8181", nil)
 }
