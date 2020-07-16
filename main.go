d0ed346fd6d00acb02932c9f627c77d879c7f86b
BeautySilver <32017851+BeautySilver@users.noreply.github.com>
2020-07-16 16:17:25 +0300

    Static files

--- main.go
+++ main.go
@@ -4,28 +4,17 @@ import (
 	"fmt"
 	"net/http"
 )
-type httpHandler struct {
-	message string
-}
 
-func (h httpHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request){
-	fmt.Fprint(resp, h.message)
-}
 func main(){
 
-	h1:=httpHandler{message:"Conact"}
+	fs:=http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
 
-	http.Handle("/contact", h1)
-
-	http.HandleFunc("/about", func(writer http.ResponseWriter, request *http.Request){
-		fmt.Fprint(writer, "About")
+	http.HandleFunc("/about", func(writer http.ResponseWriter, request *http.Request) {
+		http.ServeFile(writer, request, "static/about.html")
 	})
-	http.HandleFunc("/contct", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer, "Contact")
+	http.HandleFunc("/contact", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Contatc")
 	})
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		http.ServeFile(writer, request, "index.html")
-	})
-	fmt.Println("Listening")
 	http.ListenAndServe("localhost:8181", nil)
 }
